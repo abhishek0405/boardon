4ff679465c2984248565a1ae3785e6da23618ef1
Abhishek Anantharam <abhishekanantharam123@gmail.com>
2022-06-03 19:59:44 +0530

    mail from excel sheet functionality

--- api/controllers/credController.js
+++ api/controllers/credController.js
@@ -1,5 +1,10 @@
 const nodemailer = require("nodemailer");
 const generator = require("generate-password");
+const readxlsxFile = require("read-excel-file/node");
+const excelToJson = require("convert-excel-to-json");
+const XLSX = require("xlsx");
+const { response } = require("../../app");
+
 const transporter = nodemailer.createTransport({
   service: "gmail",
 
@@ -8,12 +13,23 @@ const transporter = nodemailer.createTransport({
     pass: process.env.PASSWORD,
   },
 });
-const generateCredentials = (req, res) => {
+const generateCredentials = async (req, res) => {
   const receiver = req.body.email;
   const company = req.body.company;
   const firstName = req.body.firstname;
   const lastName = req.body.lastname;
   const newEmail = firstName + "." + lastName + "@" + company + ".com";
+
+  if (
+    receiver == null ||
+    company == null ||
+    firstName == null ||
+    lastName == null
+  ) {
+    res.status(400).json({
+      message: "Invalid request, one or more entries is null",
+    });
+  }
   const password = generator.generate({
     length: 10,
     numbers: true,
@@ -32,7 +48,7 @@ const generateCredentials = (req, res) => {
     subject: "Boardon Credentials",
     text: body,
   };
-  transporter.sendMail(mailOptions, (error, info) => {
+  await transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.log(error);
       res.status(500).json({
@@ -46,7 +62,82 @@ const generateCredentials = (req, res) => {
   });
 };
 
-const generateCredentialsFromExcel = (req, res) => {};
+const mailCredentails = async (employee) => {
+  const receiver = employee.email;
+  const company = employee.company;
+  const firstName = employee.firstname;
+  const lastName = employee.lastname;
+  const newEmail = firstName + "." + lastName + "@" + company + ".com";
+
+  if (
+    receiver == null ||
+    company == null ||
+    firstName == null ||
+    lastName == null
+  ) {
+    return {
+      message: "Invalid request, one or more entries is null",
+    };
+  }
+  const password = generator.generate({
+    length: 10,
+    numbers: true,
+    uppercase: true,
+  });
+
+  const body = `Congratulations ${firstName} ${lastName} on getting an offer at ${company}.
+                  Here are your credentials to log in to boardon :
+                  Email : ${newEmail}
+                  Password: ${password}`;
+
+  console.log(body);
+  const mailOptions = {
+    from: process.env.EMAIL,
+    to: receiver,
+    subject: "Boardon Credentials",
+    text: body,
+  };
+
+  //Check if good to do like this,
+  try {
+    await transporter.sendMail(mailOptions);
+    return { message: "Sent successfully" };
+  } catch (error) {
+    return { message: error };
+  }
+};
+
+const generateCredentialsFromExcel = async (req, res) => {
+  const result = excelToJson({
+    sourceFile: "BoardonSheet.xlsx",
+    header: {
+      rows: 1,
+    },
+  });
+
+  for (const sheetName in result) {
+    const employeeList = result[sheetName];
+
+    const updatedEmployeeList = employeeList.map(
+      ({ A: firstname, B: lastname, C: email, D: company, ...rest }) => ({
+        firstname,
+        lastname,
+        email,
+        company,
+        ...rest,
+      })
+    );
+    console.log(updatedEmployeeList);
+    responseList = [];
+    for (employee of updatedEmployeeList) {
+      var response = await mailCredentails(employee);
+      console.log(response);
+      responseList.push(response);
+    }
+
+    return res.json(responseList);
+  }
+};
 
 module.exports = {
   generateCredentials,
