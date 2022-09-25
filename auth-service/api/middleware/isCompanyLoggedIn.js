4421008834a436600fdf3578a949277960e8dd07
Abhishek Anantharam <abhishekanantharam123@gmail.com>
2022-09-25 23:38:38 +0530

    company auth add

--- auth-service/api/middleware/isCompanyLoggedIn.js
+++ auth-service/api/middleware/isCompanyLoggedIn.js
@@ -4,7 +4,7 @@ module.exports = (req, resp, next) => {
     const token = req.cookies.authToken;
     const decoded = jwt.verify(token, process.env.JWT_KEY); //stores the token and verifies if same
     req.userData = decoded; //created new field use thhis to check if admin or Not.
-    if (decoded.isEmployee === true) {
+    if (decoded.isCompany === true) {
       next();
     } else {
       resp.json({ Message: "Unauthorized Access" });
--- auth-service/api/routes/authRoute.js
+++ auth-service/api/routes/authRoute.js
@@ -4,17 +4,28 @@ const config = require("../../config")[process.env.NODE_ENV || "development"];
 const {
   userLoginController,
   secretRouteController,
+  companyRegisterController,
+  companyLoginController,
 } = require("../controllers/authController");
 const isLoggedIn = require("../middleware/isLoggedIn");
+const isCompanyLoggedIn = require("../middleware/isCompanyLoggedIn");
 const log = config.log();
 
 router.post("/user/login", userLoginController);
+
+router.get("/user/secretRoute", isLoggedIn, secretRouteController);
+
+router.post("/company/register", companyRegisterController);
+
+router.post("/company/login", companyLoginController);
 
 //internal api call
 router.get("/user/isLoggedIn", isLoggedIn, (req, res) => {
   res.json({ ...req.userData, Authenticated: true });
 });
 
-router.get("/user/secretRoute", isLoggedIn, secretRouteController);
+router.get("/company/isLoggedIn", isCompanyLoggedIn, (req, res) => {
+  res.json(req.userData);
+});
 
 module.exports = router;
