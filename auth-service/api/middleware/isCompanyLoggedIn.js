a2244a61f75888442fc02b996fd2367b77feb3b6
Abhishek Anantharam <abhishekanantharam123@gmail.com>
2022-11-03 00:53:19 +0530

    logging tokens

--- auth-service/api/middleware/isCompanyLoggedIn.js
+++ auth-service/api/middleware/isCompanyLoggedIn.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, resp, next) => {
   try {
     const token = req.cookies.authToken;
+    console.log(token);
     const decoded = jwt.verify(token, process.env.JWT_KEY); //stores the token and verifies if same
     req.userData = decoded; //created new field use thhis to check if admin or Not.
     if (decoded.isCompany === true) {
--- email-service/index.js
+++ email-service/index.js
@@ -10,10 +10,20 @@ app.use(express.urlencoded({ extended: true }));
 const cors = require("cors");
 const host = process.env.FRONTEND || "http://localhost:3000";
 app.use(cors({ credentials: true, origin: host }));
+
 connectDB();
 
 app.use((req, res, next) => {
   log.debug(`${req.method}: ${req.url}`);
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,PUT,POST,DELETE,UPDATE,OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+  );
   return next();
 });
 app.use(cookieParser());
